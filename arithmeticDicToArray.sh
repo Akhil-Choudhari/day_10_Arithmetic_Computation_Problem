920763dd9d36e33d16e05dc8d6f76bc2c26aebd9
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-03-06 22:57:16 +0530

    read the values from dictionary to array

---
+++ arithmeticDicToArray.sh
@@ -0,0 +1,13 @@
+#!/bin bash
+
+declear -A expression
+expression[exp1]=$w;
+expression[exp2]=$x;
+expression[exp3]=$y;
+expression[exp4]=$z;
+	echo ${expression[exp1]}
+	echo ${expression[exp2]}
+	echo ${expression[exp3]}
+	echo ${expression[exp4]}
+
+
