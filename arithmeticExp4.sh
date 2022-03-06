318daf24e9ad8230cec502e85658f837eb1073f6
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-03-06 22:51:27 +0530

    computation of (a % b + c)

---
+++ arithmeticExp4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+read -p "Enter the first number:" a
+read -p "Enter the secont number:" b
+read -p "Enter the third number:" c
+	z=$(( $a % $b + $c ));
+	echo " The result of expression (a%b+c)= $z";
+
+
